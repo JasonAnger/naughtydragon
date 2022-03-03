783fae0419c714ad3208e84f5923d08204f943a6
Jason Anger <jayandn1999@gmail.com>
2022-03-03 21:06:13 +0700

    Finished

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f3b268c7ba5cb55c"; }
+/******/ 		__webpack_require__.h = function() { return "a683d22a8c05afa5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/HeadBannerGalaxy.js
+++ components/HeadBannerGalaxy.js
@@ -1,7 +1,7 @@
 
 export default function HeadBannerGalaxy() {
     return (
-        <section id="headbanner" className="galaxy flex relative pt-10 sm:pt-0" style={{height: 'calc(100vw / 16 * 9)'}}>
+        <section id="headbanner" className="galaxy flex relative pt-20 sm:pt-0" style={{height: 'calc(100vw / 16 * 9 + 5rem)'}}>
             <div className="flex top-0 left-0 relative justify-center items-center w-full overflow-hidden">
                 <img className="" src="/home/nd.webp" style={{
                     width: 'calc(38vw)',
