3f4f9466e404fe4f4c8fc3231bf67ed5c662e5ad
Jason Anger <jayandn1999@gmail.com>
2022-04-23 13:30:48 +0700

    Update Partners

--- components/Partners.js
+++ components/Partners.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function Partners() {
     const urlPartner = []
-    for(let i = 1; i<17; i++) {
+    for(let i = 1; i<19; i++) {
         if(i<10) urlPartner.push(`/partners/partner-0${i}.webp`)
         else urlPartner.push(`/partners/partner-${i}.webp`)
     }
@@ -9,11 +9,11 @@ export default function Partners() {
     return (
         <section id="partners">
             <h1 className="section-header">Partners</h1>
-            <div className="partner-list">
+            <div className="partner-list justify-center items-center">
                 {partnerList}
             </div>
             <h1 className="section-header">Team</h1>
-            <div className="partner-list justify-center">
+            <div className="partner-list justify-center items-center">
                 <div className="galaxy item"><img alt={`Naughty Dragon Partner: Galaxy Studio`} className="" src='/galaxy_studio.webp' /></div>
             </div>
         </section>
